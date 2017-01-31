21f83afca886f008ab8654bd8c66c1d8f2ab7b50
Alex Munoz <alex.munoz905@gmail.com>
2017-01-30 19:07:13 -0500

    Update WithLoop.py

--- WithLoop.py
+++ WithLoop.py
@@ -2,6 +2,6 @@ import os
 
 count = 0
 while (count < 1000):
-    inputthree = input("--> ")
-    os.system("spotify play " + inputthree)
+    input = input("--> ")
+    os.system("spotify play " + input)
     count = count + 1
